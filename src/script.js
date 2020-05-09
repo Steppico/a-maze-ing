29844dae69cb2dd57614dabb9fbdc149387bcccb
Stefano Demichelis <sdemichelis@ciandt.com>
2020-05-09 15:56:41 +0900

    issue13: implementing collisions to maze and borders.

--- src/script.js
+++ src/script.js
@@ -30,12 +30,15 @@ window.onload = () => {
 		x: (canvas.width / 2) - 50,
 		y: 0,
 		pointing: "down",
-		spin: null
+		spin: null,
+		get index() {
+			return [this.x + 50, this.y + 50];
+		}
 	}
 
 	const canvasCoords = {
 		done: false,
-	}
+	};
 
 	image = new Image();
 	image.src = arrow;
@@ -53,37 +56,40 @@ window.onload = () => {
 	}
 
 	const directions = move => {
-		if (move === 'forward') {
+		if (move === "forward") {
 			switch (movingArrow.pointing) {
 				case "down": {
-					// movingArrow.y + 200 <= element.height ? movingArrow.y += 100 : shakeIt(canvas, 'add');
-					console.log(movingArrow.y + 100);
-					console.log(movingArrow.x);
-					// console.log(canvasCoords[`${movingArrow.x} ${movingArrow.y + 200}`]);
-					canvasCoords[`${movingArrow.x} ${movingArrow.y + 100}`] ? shakeIt(canvas, 'add') : movingArrow.y += 100;
+					if (movingArrow.index[1] + 100 < 700 && !canvasCoords[`${movingArrow.x + 50} ${movingArrow.y + 100}`]) {
+						movingArrow.y += 100;
+
+					} else {
+						shakeIt(canvas, "add");
+					}
 					break;
 				};
 				case "top": {
-					console.log(movingArrow.y);
-					console.log(movingArrow.x);
-					canvasCoords[`${movingArrow.x} ${movingArrow.y}`] ? shakeIt(canvas, 'add') : movingArrow.y -= 100;
-					// movingArrow.y > 0 ? movingArrow.y -= 100 : shakeIt(canvas, 'add');
+					if (movingArrow.index[1] - 100 > 0 && !canvasCoords[`${movingArrow.x + 50} ${movingArrow.y}`]) {
+						movingArrow.y -= 100;
+					} else {
+						shakeIt(canvas, "add");
+					}
 					break;
 				};
 				case "left": {
-					console.log(movingArrow.y);
-					console.log(movingArrow.x);
-					console.log(canvasCoords[`${movingArrow.x} ${movingArrow.y}`]);
-					// movingArrow.x > 0 ? movingArrow.x -= 100 : shakeIt(canvas, 'add');
-					canvasCoords[`${movingArrow.x} ${movingArrow.y}`] ? shakeIt(canvas, 'add') : movingArrow.x -= 100;
+					if (movingArrow.index[0] - 100 > 0 && !canvasCoords[`${movingArrow.x} ${movingArrow.y + 50}`]) {
+						movingArrow.x -= 100;
+					} else {
+						shakeIt(canvas, "add");
+					}
 					break;
 				};
 				default: {
-					console.log(movingArrow.y);
-					console.log(movingArrow.x + 100);
-					console.log(canvasCoords[`${movingArrow.x + 100} ${movingArrow.y}`]);
-					// movingArrow.x + 200 <= element.width ? movingArrow.x += 100 : shakeIt(canvas, 'add');
-					canvasCoords[`${movingArrow.x + 100} ${movingArrow.y}`] ? shakeIt(canvas, 'add') : movingArrow.x += 100;
+					if (movingArrow.index[0] + 100 < 1100 && !canvasCoords[`${movingArrow.x + 100} ${movingArrow.y + 50}`]) {
+						movingArrow.x += 100;
+					} else {
+						shakeIt(canvas, "add");
+					}
+					break;
 				}
 			}
 
@@ -130,7 +136,6 @@ window.onload = () => {
 			if (i > moves.length - 1) {
 				clearInterval(intervalId);
 				movements.length = 0;
-				console.log(canvasCoords);
 			} else {
 				ctx.clearRect(0, 0, element.width, element.height);
 				draw();
@@ -143,28 +148,23 @@ window.onload = () => {
 	}
 
 	function addCanvasCoords(begin, end) {
-		console.log(begin, end);
-		if (begin[0] === end[0]) { // this means we are moving on Y axis
+		if (begin[0] === end[0]) { //--> this means we are moving on Y axis
 			if (begin[1] > end[1]) {
-				console.log("Y Axis, bottom to top");
-				for (let i = end[1]; i <= begin[1]; i = i + 100) {
+				for (let i = end[1]; i <= begin[1]; i++) {
 					canvasCoords[`${begin[0]} ${i}`] = [begin[0], i];
 				}
 			} else {
-				console.log("Y axis, top to bottom");
-				for (let i = begin[1]; i <= end[1]; i = i + 100) {
+				for (let i = begin[1]; i <= end[1]; i++) {
 					canvasCoords[`${begin[0]} ${i}`] = [begin[0], i];
 				}
 			}
-		} else if (begin[1] === end[1]) { // this means we are moving on X axis
+		} else if (begin[1] === end[1]) { //--> this means we are moving on X axis
 			if (begin[0] > end[0]) {
-				console.log("X Axis, rtl");
-				for (let j = end[0]; j <= begin[0]; j = j + 100) {
+				for (let j = end[0]; j <= begin[0]; j++) {
 					canvasCoords[`${j} ${end[1]}`] = [j, end[1]];
 				}
 			} else {
-				console.log("X Axis, ltr")
-				for (let j = begin[0]; j <= end[0]; j = j + 100) {
+				for (let j = begin[0]; j <= end[0]; j++) {
 					canvasCoords[`${j} ${end[1]}`] = [j, end[1]];
 				}
 			}
@@ -172,78 +172,98 @@ window.onload = () => {
 	}
 
 	function draw() {
-		// PoC with canvas borders
-		addCanvasCoords([0, 0], [element.width, 0]);
-		addCanvasCoords([element.width, 0], [element.width, element.height]);
-		addCanvasCoords([element.width, element.height], [0, element.height]);
-		addCanvasCoords([0, element.height], [0, 0]);
-		console.log(canvasCoords);
+
 		ctx.lineWidth = 5;
 		ctx.beginPath();
 
 		// top (entrance from 500 to 600)
 		ctx.moveTo(100, 100);
 		ctx.lineTo(500, 100);
-		// canvasCoords.done ? "" : addCanvasCoords([100, 100], [500, 100]);
+		addCanvasCoords([100, 100], [500, 100]);
 		ctx.moveTo(600, 100);
 		ctx.lineTo(1000, 100);
+		addCanvasCoords([600, 100], [1000, 100]);
 
 		// left side
 		ctx.moveTo(100, 97.5);
 		ctx.lineTo(100, element.height - 100);
+		addCanvasCoords([100, 100], [100, element.height - 100]);
+
 
 		// bottom (exit from 500 to 600)
 		ctx.lineTo(500, element.height - 100);
+		addCanvasCoords([100, element.height - 100], [500, element.height - 100]);
 		ctx.moveTo(600, element.height - 100);
 		ctx.lineTo(1000, element.height - 100);
-
+		addCanvasCoords([600, element.height - 100], [1000, element.height - 100]);
 		// right side
 		ctx.lineTo(1000, 97.5);
+		addCanvasCoords([1000, element.height - 100], [1000, 100]);
 
 		// inside pattern
 		//// horizontal lines
 		ctx.moveTo(100, 200);
 		ctx.lineTo(200, 200);
+		addCanvasCoords([100, 200], [200, 200]);
 		ctx.moveTo(300, 200);
 		ctx.lineTo(700, 200);
+		addCanvasCoords([300, 200], [700, 200]);
 		ctx.moveTo(800, 200);
 		ctx.lineTo(900, 200);
+		addCanvasCoords([800, 200], [900, 200]);
 		ctx.moveTo(400, 300);
 		ctx.lineTo(500, 300);
+		addCanvasCoords([400, 300], [500, 300]);
 		ctx.moveTo(700, 300);
 		ctx.lineTo(800, 300);
+		addCanvasCoords([700, 300], [800, 300]);
 		ctx.moveTo(900, 300);
 		ctx.lineTo(1000, 300);
+		addCanvasCoords([900, 300], [1000, 300]);
 		ctx.moveTo(200, 400);
 		ctx.lineTo(400, 400);
+		addCanvasCoords([200, 400], [400, 400]);
 		ctx.moveTo(500, 400);
 		ctx.lineTo(700, 400);
+		addCanvasCoords([500, 400], [700, 400]);
 		ctx.moveTo(900, 400);
 		ctx.lineTo(1000, 400);
+		addCanvasCoords([900, 400], [1000, 400]);
 		ctx.moveTo(100, 500);
 		ctx.lineTo(300, 500);
+		addCanvasCoords([100, 500], [300, 500]);
 		ctx.moveTo(500, 500);
 		ctx.lineTo(600, 500);
+		addCanvasCoords([500, 500], [600, 500]);
 		ctx.moveTo(700, 500);
 		ctx.lineTo(900, 500);
+		addCanvasCoords([700, 500], [900, 500]);
 
 		//// vertical lines
 		ctx.moveTo(200, 300);
 		ctx.lineTo(200, 402.5);
+		addCanvasCoords([200, 300], [200, 400]);
 		ctx.moveTo(300, 197.5);
 		ctx.lineTo(300, 300);
+		addCanvasCoords([300, 200], [300, 300]);
 		ctx.moveTo(400, 297.5);
 		ctx.lineTo(400, 600);
+		addCanvasCoords([400, 300], [300, 600]);
 		ctx.moveTo(500, 397.5);
 		ctx.lineTo(500, 502.5);
+		addCanvasCoords([500, 400], [500, 500]);
 		ctx.moveTo(600, 200);
 		ctx.lineTo(600, 400);
+		addCanvasCoords([600, 200], [600, 400]);
 		ctx.moveTo(600, 497.5);
 		ctx.lineTo(600, 602.5);
+		addCanvasCoords([600, 500], [600, 600]);
 		ctx.moveTo(700, 397.5);
 		ctx.lineTo(700, 502.5);
+		addCanvasCoords([700, 400], [700, 500]);
 		ctx.moveTo(800, 100);
 		ctx.lineTo(800, 400);
+		addCanvasCoords([800, 100], [800, 400]);
 		// drawing
 		ctx.stroke();
 	}
